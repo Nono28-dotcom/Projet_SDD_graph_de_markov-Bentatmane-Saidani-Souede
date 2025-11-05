284849e2adb383a9fda82c4abc55b7f29ffec7e2
Laetitia208 <laetitiabenatmane@gmail.com>
2025-11-05 17:58:41 +0100

    Écriture des structures

--- markov.h
+++ markov.h
@@ -7,6 +7,20 @@
 
 #include <stdio.h>
 
+typedef struct cell{
+  int sommet_arrivee;
+  float Proba;
+  cell *next;
+}cell;
+
+typedef struct liste {
+  cell *head;
+}liste;
+
+typedef struct liste_adjacence {
+  liste *tab;
+  int taille;
+}liste_adjacence;
 
 
 #endif //MARKOV_H
