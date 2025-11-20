615d88d0e05d383173a69dc3cbeccb72b192ba33
lydiasaidani <lydia.saidani@efrei.net>
2025-11-20 20:12:41 +0100

    mo

--- markov.h
+++ markov.h
@@ -31,6 +31,45 @@ void afficher_liste_adjacence(liste_adjacence adj);
 liste_adjacence readGraph (const char* filename);
 int verif_graphe_Markov(liste_adjacence adj);
 int export_to_mermaid(const liste_adjacence *adj, const char *filename);
+typedef struct s_tarjan_vertex{
+    int id;
+    int num;
+    int numacc;
+    int indic;
+}t_tarjan_vertex;
+
+t_tarjan_vertex *T;
+typedef struct t_classe{
+    char nom_classe[100];
+    t_tarjan_vertex ** Tsommets;
+    int capacite_c;
+    int nbsommets;
+}t_classe;
+typedef struct t_partition{
+    t_classe ** Tclasses;
+    int capacite_p;
+    int nbclasses;
+}t_partition;
+t_tarjan_vertex * init_vertex ( liste_adjacence adj){
+    int n= adj.taille;
+    t_tarjan_vertex * T=malloc(n*sizeof(t_tarjan_vertex));
+    for (int i=0; i<n; i++){
+        T[i].id=i+1;
+        T[i].num= -1;
+        T[i].numacc=-1;
+        T[i].indic=0;
+    }
+    return T;
+}
+typedef struct s_cell {
+    int value;
+    struct s_cell *next;
+}t_cell;
+typedef struct s_list {
+    t_cell * head;
+}t_list;
+typedef t_list t_stacklist;
+
 
 
 
