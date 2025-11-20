f00d9ebeea2940e9b1335d0712d642d095c8ca8e
nsoue <noah.souede@efrei.net>
2025-11-21 00:44:29 +0100

    commentaires

---
+++ matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by nsoue on 21/11/2025.
+//
+
+#include "matrix.h"
---
+++ matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by nsoue on 21/11/2025.
+//
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#endif //MATRIX_H
